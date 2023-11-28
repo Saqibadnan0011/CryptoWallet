5d0d8efa2b2a6935b0f98cc796cdbb64353efc50
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-29 01:14:43 +0500

    HomeView

---
+++ CryptoWallet/Core/Home/View/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  CryptoWallet
+//
+//  Created by apple on 29/11/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Crypto Wallet")
+                Spacer()
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
