46e43665e1e6ee338597f836107291a54abf8150
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-29 00:59:49 +0500

    Add color them

---
+++ CryptoWallet/Extension/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CrytoWallet
+//
+//  Created by apple on 29/11/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let dark = Color("DarkGreen")
+    let secondaryText = Color("SecondaryTextColor")
+}
