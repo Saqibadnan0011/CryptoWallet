bdfb51f1fbde533ca18fc6121009647b20be2153
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-28 23:42:20 +0500

    Initial commit

---
+++ CrytoWallet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CrytoWallet
+//
+//  Created by apple on 28/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CrytoWallet/CrytoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrytoWalletApp.swift
+//  CrytoWallet
+//
+//  Created by apple on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrytoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
