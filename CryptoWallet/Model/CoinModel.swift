f37c468a2faf32482b0bb4661489c529c8bfad1f
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-01 01:01:29 +0500

    Adding API

---
+++ CryptoWallet/Model/CoinModel.swift
@@ -0,0 +1,56 @@
+//
+//  CoinModel.swift
+//  CryptoWallet
+//
+//  Created by apple on 01/12/2023.
+//
+
+import Foundation
+
+//Coingecko API
+/*
+ URL:
+   https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en
+ 
+ JSON Response:
+ {
+     "id": "bitcoin",
+     "symbol": "btc",
+     "name": "Bitcoin",
+     "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+     "current_price": 37792,
+     "market_cap": 739082829935,
+     "market_cap_rank": 1,
+     "fully_diluted_valuation": 793604924280,
+     "total_volume": 16836569096,
+     "high_24h": 38144,
+     "low_24h": 37547,
+     "price_change_24h": -38.78566674711328,
+     "price_change_percentage_24h": -0.10253,
+     "market_cap_change_24h": -1183697689.0709229,
+     "market_cap_change_percentage_24h": -0.1599,
+     "circulating_supply": 19557262,
+     "total_supply": 21000000,
+     "max_supply": 21000000,
+     "ath": 69045,
+     "ath_change_percentage": -45.27669,
+     "ath_date": "2021-11-10T14:24:11.849Z",
+     "atl": 67.81,
+     "atl_change_percentage": 55620.60341,
+     "atl_date": "2013-07-06T00:00:00.000Z",
+     "roi": null,
+     "last_updated": "2023-11-30T19:51:53.431Z",
+     "sparkline_in_7d": {
+       "price": [
+         37153.120825669925,
+         37256.538097687306,
+         37314.64674858196,
+       ]
+     },
+     "price_change_percentage_24h_in_currency": -0.10252518546821178
+   },
+ */
+
+class CoinModel {
+    
+}
