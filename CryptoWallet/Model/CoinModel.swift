811313abe002190cde375e659556710d92268519
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-01 21:05:25 +0500

    API Integration

--- CryptoWallet/Model/CoinModel.swift
+++ CryptoWallet/Model/CoinModel.swift
@@ -101,6 +101,14 @@ struct CoinModel: Identifiable, Codable {
     func updateHoldings(amount: Double) -> CoinModel {
         return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparkLineIn7D: sparkLineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amount)
     }
+    
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    var rank: Int {
+        return Int(marketCapRank ?? 0)
+    }
 }
 
 struct SparkLineIn7D: Codable {
