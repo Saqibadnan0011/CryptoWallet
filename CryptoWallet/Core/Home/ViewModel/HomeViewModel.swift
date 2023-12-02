d367224f7ee5c78764afc79b2eb3aa4c0c463a0c
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-02 16:43:51 +0500

    Calling API

--- CryptoWallet/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoWallet/Core/Home/ViewModel/HomeViewModel.swift
@@ -6,16 +6,24 @@
 //
 
 import Foundation
-import SwiftUI
+import Combine
 
 class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCons: [CoinModel] = []
     
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCons.append(DeveloperPreview.instance.coin)
+        addSubscribers()
         }
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
+    
