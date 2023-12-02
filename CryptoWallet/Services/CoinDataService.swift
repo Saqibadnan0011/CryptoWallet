9a76a7ff588c15a48a124a36162ff37de4b74fb4
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-02 16:49:10 +0500

    Integration API

---
+++ CryptoWallet/Services/CoinDataService.swift
@@ -0,0 +1,51 @@
+//
+//  CoinDataService.swift
+//  CryptoWallet
+//
+//  Created by apple on 02/12/2023.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    
+    var coinSubscription: AnyCancellable?
+    
+    init () {
+        getCoin()
+    }
+    
+    private func getCoin() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en")
+        else { return }
+        
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink { (completion) in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self](returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            }
+            //.store(in: &cancellables)
+
+    }
+}
