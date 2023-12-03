db29c582065a5a3b64a184955c363594fe309125
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-04 00:46:56 +0500

    CryptoWallet: Modified services

--- CryptoWallet/Services/CoinImageService.swift
+++ CryptoWallet/Services/CoinImageService.swift
@@ -15,13 +15,27 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getIamge(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from file Manager!")
+        } else {
+            downloadCoinImage()
+            print("Download image now!")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkManager.download(url: url)
@@ -29,8 +43,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
