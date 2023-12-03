843804ead0bd7ede4a498deb1de440ef3b31691a
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-04 01:44:17 +0500

    CryptoWallet: Added the market services

---
+++ CryptoWallet/Services/MarketDataService.swift
@@ -0,0 +1,32 @@
+//
+//  MarketDataService.swift
+//  CryptoWallet
+//
+//  Created by apple on 04/12/2023.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    
+    var marketDataSubscription: AnyCancellable?
+    
+    init () {
+        getData()
+    }
+    
+    private func getData() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkManager.download(url: url)
+            .decode(type: GlobalData.self , decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
