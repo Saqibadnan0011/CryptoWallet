9e50251074123d786548bbdb096d0419fb74265f
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-04 00:47:46 +0500

    CryptoWallet: Add statistics in HomeViewModel

--- CryptoWallet/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoWallet/Core/Home/ViewModel/HomeViewModel.swift
@@ -10,8 +10,16 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
+    @Published var statistics: [Statistics] = [
+        Statistics(title: "title", value: "value", percentageChange: 1),
+        Statistics(title: "title", value: "value"),
+        Statistics(title: "title", value: "value"),
+        Statistics(title: "title", value: "value", percentageChange: -5)
+    ]
+    
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    @Published var searchText: String = "" 
     
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
@@ -21,11 +29,28 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        //updated coins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coin: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coin
+        }
+        let lowercasedText = text.lowercased()
+        
+        return coin.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
     
