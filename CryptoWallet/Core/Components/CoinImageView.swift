e6236280793ce9094f347da0686643855bf3c223
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-03 15:53:44 +0500

    Crypto Wallet: Coin Image View

---
+++ CryptoWallet/Core/Components/CoinImageView.swift
@@ -0,0 +1,38 @@
+//
+//  CoinImageView.swift
+//  CryptoWallet
+//
+//  Created by apple on 03/12/2023.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init (coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView(coin: dev.coin)
+    }
+}
