cc8d1944f8f031c7492d0d3c24ff17ff4fc080e8
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-03 15:55:54 +0500

    Crypto Wallet: Subscribe the coin image services

---
+++ CryptoWallet/Services/CoinImageService.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageService.swift
+//  CryptoWallet
+//
+//  Created by apple on 03/12/2023.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
+        
+        imageSubscription = NetworkManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
