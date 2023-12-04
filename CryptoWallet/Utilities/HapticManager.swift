074098ecb7e6fe088c1a5acf1f9844ba10c55676
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-05 03:39:37 +0500

    CryptoWallet: Updated the Application

---
+++ CryptoWallet/Utilities/HapticManager.swift
@@ -0,0 +1,19 @@
+//
+//  HapticManager.swift
+//  CryptoWallet
+//
+//  Created by apple on 04/12/2023.
+//
+
+import Foundation
+import SwiftUI
+
+class HapticManager {
+    
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        generator.notificationOccurred(type)
+    }
+}
+
