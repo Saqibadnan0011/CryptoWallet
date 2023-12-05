43037dfcb74b8885ed17b0a88ff5e639cf47055e
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-12-06 03:06:16 +0500

    CryptoWallet: create Date model to fetch the current date

---
+++ CryptoWallet/Extension/Date.swift
@@ -0,0 +1,28 @@
+//
+//  Date.swift
+//  CryptoWallet
+//
+//  Created by apple on 05/12/2023.
+//
+
+import Foundation
+
+extension Date {
+    
+    init(coinGeckoString: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let date = formatter.date(from: coinGeckoString) ?? Date()
+        self.init(timeInterval: 0, since: date)
+    }
+    
+    private var shortFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }
+    
+    func asShortDateString() -> String {
+        return shortFormatter.string(from: self)
+    }
+}
